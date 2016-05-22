649db7724c07258bb2cc8a81f49082c7bd52734c
Paul <ppruneta@gmail.com>
2016-05-22 12:19:04 +0200

    added package.json + Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+	
+	//config
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+			  mangle: false
+			},
+			my_target: {
+				files: {
+					'dist/flip-book.min.js': ['dist/flip-book.js']
+				}
+			}
+		}
+	});
+	
+	//load uglify
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+		
+	//tâche
+	grunt.registerTask('default', ['uglify']);
+}
