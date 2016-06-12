6310875fb16586a48a8026a9d693dec7d008d20f
Paul <ppruneta@gmail.com>
2016-06-12 20:10:24 +0200

    added CustomEvent() polyfill for ie

--- dist/flip-book.js
+++ dist/flip-book.js
@@ -82,6 +82,23 @@ function Flip(options)
 		loops = totalNumber*repeat;
 	}
 	//EVENTS
+	//CustomEvent() polyfill for ie
+	(function () {
+
+	  if ( typeof window.CustomEvent === "function" ) return false;
+
+	  function CustomEvent ( event, params ) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		var evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	   }
+
+	  CustomEvent.prototype = window.Event.prototype;
+
+	  window.CustomEvent = CustomEvent;
+	})();
+	//events
 	var completeEvent = new CustomEvent('complete',{detail:{container: container, src: src}});
 	var startEvent = new CustomEvent('start',{detail:{container: container, src: src}});
 	var pauseEvent = new CustomEvent('pause',{detail:{container: container, src: src}});
